8073cdfde76710b946fa4787cf7106b8e2574418
zyxkad <zyxkad@gmail.com>
2026-05-01 20:49:48 +0800

    add sha1

--- hash/sha1.lua
+++ hash/sha1.lua
@@ -3,12 +3,16 @@
 -- license that can be found in the LICENSE file.
 -- Translate by zyxkad@gmail.com
 --
--- sha256 hash API
+-- sha1 hash API
 
 local expect = require('cc.expect')
 
 local band, bor, bxor, bnot = bit32.band, bit32.bor, bit32.bxor, bit32.bnot
-local brrotate, blshift, brshift = bit32.rrotate, bit32.lshift, bit32.rshift
+local blrotate, blshift, brshift = bit32.lrotate, bit32.lshift, bit32.rshift
+
+local function low4bits(n)
+	return band(n, 0xf)
+end
 
 local function uint32(n)
 	return band(n, 0xffffffff)
@@ -38,129 +42,71 @@ local zeroArrMt = {
 	end,
 }
 
-local SIZE = 32
+local SIZE = 20
 local BLOCK_SIZE = 64
 
 local chunk = 64
 local chunkMaskInv = bnot(chunk - 1)
 
-local init1 = 0x6A09E667
-local init2 = 0xBB67AE85
-local init3 = 0x3C6EF372
-local init4 = 0xA54FF53A
-local init5 = 0x510E527F
-local init6 = 0x9B05688C
-local init7 = 0x1F83D9AB
-local init8 = 0x5BE0CD19
+local init1 = 0x67452301
+local init2 = 0xEFCDAB89
+local init3 = 0x98BADCFE
+local init4 = 0x10325476
+local init5 = 0xC3D2E1F0
 
-local _K = {
-	0x428a2f98,
-	0x71374491,
-	0xb5c0fbcf,
-	0xe9b5dba5,
-	0x3956c25b,
-	0x59f111f1,
-	0x923f82a4,
-	0xab1c5ed5,
-	0xd807aa98,
-	0x12835b01,
-	0x243185be,
-	0x550c7dc3,
-	0x72be5d74,
-	0x80deb1fe,
-	0x9bdc06a7,
-	0xc19bf174,
-	0xe49b69c1,
-	0xefbe4786,
-	0x0fc19dc6,
-	0x240ca1cc,
-	0x2de92c6f,
-	0x4a7484aa,
-	0x5cb0a9dc,
-	0x76f988da,
-	0x983e5152,
-	0xa831c66d,
-	0xb00327c8,
-	0xbf597fc7,
-	0xc6e00bf3,
-	0xd5a79147,
-	0x06ca6351,
-	0x14292967,
-	0x27b70a85,
-	0x2e1b2138,
-	0x4d2c6dfc,
-	0x53380d13,
-	0x650a7354,
-	0x766a0abb,
-	0x81c2c92e,
-	0x92722c85,
-	0xa2bfe8a1,
-	0xa81a664b,
-	0xc24b8b70,
-	0xc76c51a3,
-	0xd192e819,
-	0xd6990624,
-	0xf40e3585,
-	0x106aa070,
-	0x19a4c116,
-	0x1e376c08,
-	0x2748774c,
-	0x34b0bcb5,
-	0x391c0cb3,
-	0x4ed8aa4a,
-	0x5b9cca4f,
-	0x682e6ff3,
-	0x748f82ee,
-	0x78a5636f,
-	0x84c87814,
-	0x8cc70208,
-	0x90befffa,
-	0xa4506ceb,
-	0xbef9a3f7,
-	0xc67178f2,
-}
+local _K0 = 0x5A827999
+local _K1 = 0x6ED9EBA1
+local _K2 = 0x8F1BBCDC
+local _K3 = 0xCA62C1D6
 
 local function block(digH, p, pStart, pEnd)
-	local w = {} -- [64]uint32
-	local h1, h2, h3, h4, h5, h6, h7, h8 = digH[1], digH[2], digH[3], digH[4], digH[5], digH[6], digH[7], digH[8]
+	local w = {} -- [16]uint32
+	local h1, h2, h3, h4, h5 = digH[1], digH[2], digH[3], digH[4], digH[5]
 
 	while pEnd - pStart + 1 >= chunk do
 		for i = 1, 16 do
 			local j = pStart + (i - 1) * 4
 			w[i] = bor(blshift(p[j + 0], 24), blshift(p[j + 1], 16), blshift(p[j + 2], 8), p[j + 3])
 		end
-		for i = 17, 64 do
-			local v1 = w[i-2]
-			local t1 = bxor(brrotate(v1, 17), brrotate(v1, 19), brshift(v1, 10))
-			local v2 = w[i-15]
-			local t2 = bxor(brrotate(v2, 7), brrotate(v2, 18), brshift(v2, 3))
-			w[i] = uint32(t1 + w[i-7] + t2 + w[i-16])
+
+		local a, b, c, d, e = h1, h2, h3, h4, h5
+
+		-- Each of the four 20-iteration rounds
+		-- differs only in the computation of f and
+		-- the choice of K (_K0, _K1, etc).
+		for i = 1, 16 do
+			local f = bor(band(b, c), band(bnot(b), d))
+			local t = uint32(blrotate(a, 5) + f + e + w[i] + _K0)
+			a, b, c, d, e = t, a, blrotate(b, 30), c, d
 		end
+		for i = 16, 19 do
+			local tmp = bxor(w[1 + low4bits(i-3)], w[1 + low4bits(i-8)], w[1 + low4bits(i-14)], w[1 + low4bits(i)])
+			w[1 + low4bits(i)] = blrotate(tmp, 1)
 
-		local a, b, c, d, e, f, g, h = h1, h2, h3, h4, h5, h6, h7, h8
-
-		for i = 1, 64 do
-			local t1 = uint32(
-				h +
-				bxor(brrotate(e, 6), brrotate(e, 11), brrotate(e, 25)) +
-				bxor(band(e, f), band(bnot(e), g)) +
-				_K[i] +
-				w[i]
-			)
-
-			local t2 = uint32(
-				bxor(brrotate(a, 2), brrotate(a, 13), brrotate(a, 22)) +
-				bxor(band(a, b), band(a, c), band(b, c))
-			)
-
-			h = g
-			g = f
-			f = e
-			e = uint32(d + t1)
-			d = c
-			c = b
-			b = a
-			a = uint32(t1 + t2)
+			local f = bor(band(b, c), band(bnot(b), d))
+			local t = uint32(blrotate(a, 5) + f + e + w[1 + low4bits(i)] + _K0)
+			a, b, c, d, e = t, a, blrotate(b, 30), c, d
+		end
+		for i = 20, 39 do
+			local tmp = bxor(w[1 + low4bits(i-3)], w[1 + low4bits(i-8)], w[1 + low4bits(i-14)], w[1 + low4bits(i)])
+			w[1 + low4bits(i)] = blrotate(tmp, 1)
+			local f = bxor(b, c, d)
+			local t = uint32(blrotate(a, 5) + f + e + w[1 + low4bits(i)] + _K1)
+			a, b, c, d, e = t, a, blrotate(b, 30), c, d
+		end
+		for i = 40, 59 do
+			local tmp = bxor(w[1 + low4bits(i-3)], w[1 + low4bits(i-8)], w[1 + low4bits(i-14)], w[1 + low4bits(i)])
+			w[1 + low4bits(i)] = blrotate(tmp, 1)
+			local f = bor(band(bor(b, c), d), band(b, c))
+			local t = uint32(blrotate(a, 5) + f + e + w[1 + low4bits(i)] + _K2)
+			a, b, c, d, e = t, a, blrotate(b, 30), c, d
+		end
+		for i = 60, 79 do
+			local tmp = bxor(w[1 + low4bits(i-3)], w[1 + low4bits(i-8)], w[1 + low4bits(i-14)], w[1 + low4bits(i)])
+			w[1 + low4bits(i)] = blrotate(tmp, 1)
+			local f = bxor(b, c, d)
+			local t = uint32(blrotate(a, 5) + f + e + w[1 + low4bits(i)] + _K3)
+			a, b, c, d, e = t, a, blrotate(b, 30), c, d
 		end
 
 		h1 = uint32(h1 + a)
@@ -168,14 +114,11 @@ local function block(digH, p, pStart, pEnd)
 		h3 = uint32(h3 + c)
 		h4 = uint32(h4 + d)
 		h5 = uint32(h5 + e)
-		h6 = uint32(h6 + f)
-		h7 = uint32(h7 + g)
-		h8 = uint32(h8 + h)
 
 		pStart = pStart + chunk
 	end
 
-	digH[1], digH[2], digH[3], digH[4], digH[5], digH[6], digH[7], digH[8] = h1, h2, h3, h4, h5, h6, h7, h8
+	digH[1], digH[2], digH[3], digH[4], digH[5] = h1, h2, h3, h4, h5
 end
 
 local function newDigest()
@@ -196,9 +139,6 @@ local function newDigest()
 		h[3] = init3
 		h[4] = init4
 		h[5] = init5
-		h[6] = init6
-		h[7] = init7
-		h[8] = init8
 		digest._nx = 0
 		digest._len = 0
 	end
@@ -268,7 +208,7 @@ local function newDigest()
 		digest.write(padlen)
 
 		local h = digest._h
-		return string.pack('>IIIIIIII', h[1], h[2], h[3], h[4], h[5], h[6], h[7], h[8])
+		return string.pack('>IIIII', h[1], h[2], h[3], h[4], h[5])
 	end
 
 	return digest
---
+++ test/hash/sha1_test.lua
@@ -0,0 +1,49 @@
+
+package.path = package.path .. ';../../?;../../?.lua;../../?/init.lua'
+
+local sha1 = require('hash.sha1')
+
+local byte2hex = {}
+for i = 0, 255 do
+	local b = string.char(i)
+	local h = string.format('%02x', i)
+	byte2hex[b] = h
+end
+
+local function bytesToHex(s)
+	local res = s:gsub('.', byte2hex)
+	return res
+end
+
+local tests = {
+	{'', 'da39a3ee5e6b4b0d3255bfef95601890afd80709'},
+	{'abc', 'a9993e364706816aba3e25717850c26c9cd0d89d'},
+	{'advancedperipherals', 'd39662afd96a61bf6ef170dd1628eb3041076038'},
+	{'\x00', '5ba93c9db0cff93f52b521d7420e43f6eda2784f'},
+	{
+		'a very very very very very very very very very very very very very very very very long text',
+		'eb7fc66067fe4bcc4ed481d96ce8c6d054cf94df',
+	},
+	{
+		't h i s   t e x t   i s   e x a c t l y   56   bytes....',
+		'7d9c153786fb36860be178b82866c0af41c37109',
+	},
+	{
+		'super looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong text',
+		'80a69c320683697ea371eaefb0c030915771995b',
+	},
+}
+
+local passed = 0
+
+for i, data in ipairs(tests) do
+	local input, expect = data[1], data[2]
+	local o = bytesToHex(sha1(input))
+	if o ~= expect then
+		printError(string.format('[%d] sha1 for %q is %s, expect %s', i, input, o, expect))
+	else
+		passed = passed + 1
+	end
+end
+
+print(string.format('Passed tests %d / %d', passed, #tests))
